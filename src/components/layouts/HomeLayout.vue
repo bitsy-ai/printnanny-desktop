d014e4abd824f5ccda03142cd30a8418c213740f
Leigh Johnson <leigh@bitsy.ai>
2023-04-13 13:25:57 -0700

    fix: primary layout after login

--- src/components/layouts/HomeLayout.vue
+++ src/components/layouts/HomeLayout.vue
@@ -272,8 +272,8 @@ import {
   XMarkIcon,
 } from "@heroicons/vue/24/outline";
 import { ChevronDownIcon, MagnifyingGlassIcon } from "@heroicons/vue/20/solid";
-import DesktopNavbar from "@/components/DesktopNavbar.vue";
-import { NAVIGATION_URLS } from "../data/urls";
+import DesktopNavbar from "@/components/nav/DesktopNavbar.vue";
+import { NAVIGATION_URLS } from "@/data/urls";
 
 const sidebarOpen = ref(false);
 </script>
