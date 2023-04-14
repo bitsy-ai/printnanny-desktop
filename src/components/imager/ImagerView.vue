212ed336eecb9e3d56d0b88ef5dd5788464b2413
Leigh Johnson <leigh@bitsy.ai>
2023-04-14 16:22:24 -0700

    wip

--- src/components/imager/ImagerView.vue
+++ src/components/imager/ImagerView.vue
@@ -19,12 +19,12 @@ onMounted(() => {
     class="h-full grid grid-rows-3 bg-zinc-500 items-center content-center justify-content-center space-y-20"
   >
     <nav aria-label="Progress">
-      <ol role="list" class="flex items-center mt-6 justify-center">
+      <ol role="list" class="flex items-center mt-6 justify-center ml-4">
         <li
           v-for="(step, stepIdx) in store.steps"
           :key="step.name"
           :class="[
-            stepIdx !== store.steps.length - 1 ? 'pr-56' : '',
+            stepIdx !== store.steps.length - 1 ? 'pr-52' : '',
             'relative',
           ]"
         >
@@ -36,11 +36,11 @@ onMounted(() => {
               <div class="h-0.5 w-full bg-indigo-400" />
             </div>
             <span
-              class="relative flex h-12 w-12 items-center justify-center rounded-full bg-indigo-400"
+              class="relative flex h-20 w-20 items-center justify-center rounded-full bg-indigo-400"
             >
               <component
                 :is="step.solidIcon"
-                class="h-8 w-8 text-white"
+                class="h-16 w-16 text-white"
               ></component>
               <span class="sr-only">{{ step.name }}</span>
             </span>
@@ -50,12 +50,12 @@ onMounted(() => {
               <div class="h-0.5 w-full bg-gray-200" />
             </div>
             <span
-              class="relative flex h-12 w-12 items-center justify-center rounded-full border-4 border-indigo-400 bg-white"
+              class="relative flex h-20 w-20 items-center justify-center rounded-full border-4 border-indigo-400 bg-white"
               aria-current="step"
             >
               <component
                 :is="step.solidIcon"
-                class="h-8 w-8 text-indigo-400"
+                class="h-16 w-16 text-indigo-400"
               ></component>
               <span class="sr-only">{{ step.name }}</span>
             </span>
@@ -65,15 +65,17 @@ onMounted(() => {
               <div class="h-0.5 w-full bg-gray-200" />
             </div>
             <span
-              class="group relative flex h-12 w-12 items-center justify-center rounded-full border-2 border-gray-300 bg-white border-gray-400"
+              class="group relative flex h-20 w-20 items-center justify-center rounded-full border-2 border-gray-300 bg-white border-gray-400"
             >
               <component
                 :is="step.solidIcon"
-                class="h-8 w-8 text-gray-400"
+                class="h-16 w-16 text-gray-400"
               ></component>
               <span class="sr-only">{{ step.name }}</span>
             </span>
           </template>
+
+          
         </li>
       </ol>
     </nav>
