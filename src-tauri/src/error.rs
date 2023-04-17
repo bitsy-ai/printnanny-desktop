82add0544dfd22ddab4b90e4159046840b2b0b3f
Leigh Johnson <leigh@bitsy.ai>
2023-04-17 12:44:24 -0700

    wip: porting imager back-end

---
+++ src-tauri/src/error.rs
@@ -0,0 +1,38 @@
+use serde::{Deserialize, Serialize};
+use std::io;
+use thiserror::Error;
+
+// user-facing error messages
+// more verbose info is logged at the error!() level
+#[derive(Error, Debug, Serialize)]
+pub enum ImagerError {
+    #[error(
+        "Failed to write boot file {path}. Please eject the device, re-insert, and try again."
+    )]
+    BootfileWrite { path: String },
+    #[error("Failed to unmount volume. Please eject the device, re-insert, and try again.")]
+    VolumeUnmount,
+    #[error("Failed to acquire exclusive lock for device. Please eject the device, re-insert, and try again.")]
+    VolumeLock,
+    #[error("Failed to format disk. Please eject the device, re-insert, and try again.")]
+    FormatDisk,
+    #[error("Failed to open disk {path}. Please eject the device, re-insert, and try again.")]
+    OpenDisk { path: String },
+
+    #[error("I/O buffer operation failed with error: {msg}")]
+    BufferIoError { msg: String },
+
+    #[error("Command {cmd} failed with error: {msg}")]
+    CommandIoError { msg: String, cmd: String },
+
+    #[error("JSON serialization failed with error: {msg}")]
+    SerdeJson { msg: String },
+
+    #[cfg(target_os = "macos")]
+    #[error("MacOS Security Framework failed with error: {msg}")]
+    MacOsSecurityFrameworkError { msg: String },
+
+    #[cfg(unix)]
+    #[error("Unix socket communication failed with Errno: {errno}")]
+    UnixSocketErro { errno: String },
+}
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,26 @@
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-async fn imager_list_diskdrives() -> String {
-    // let disks = disklist::list_disks().await.unwrap();
-    // serde_json::to_string(&disks).unwrap()
-    "".into()
-}
+
+pub mod app;
+pub mod bootfile;
+pub mod disks;
+pub mod error;
+pub mod imager;
+pub mod password;
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![imager_list_diskdrives])
+        // cheap dirty hack: create a global, mutable singleton OnceCell to track image progress completion
+        .setup(|app| {
+            app::TauriApp::set(app.handle());
+            Ok(())
+        })
+        .invoke_handler(tauri::generate_handler![
+            disks::list_diskdrives,
+            imager::write_image,
+            password::hash_password,
+            password::compare_password,
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
